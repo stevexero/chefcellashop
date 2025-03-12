5859293b25f782d5ee1d37ec54e9285c19c64201
stevexero <steveanthony999@gmail.com>
2025-03-12 03:18:49 -0700

    Built navbar and other basic setup

---
+++ app/components/navbar.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import TopNavLinks from './top-nav-links';
+
+const Navbar = () => {
+  return (
+    <div className='w-screen'>
+      <div className='w-full p-4 grid grid-cols-3 justify-items-center items-center bg-black'>
+        <p className='text-white text-xs'>SHIPPING INCLUDED WITH ALL ORDERS!</p>
+        <Image
+          src='/frontlogocut.png'
+          width={100}
+          height={100}
+          alt='chef cella logo'
+        />
+        <TopNavLinks />
+      </div>
+      <div className='w-full p-2 border-b border-b-slate-300 text-center'>
+        All orders processed and shipped the same day!
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/components/top-nav-links.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { CiUser, CiHeart, CiShoppingCart } from 'react-icons/ci';
+
+const TopNavLinks = () => {
+  return (
+    <div className='text-white flex flex-row items-center'>
+      <CiUser size='1.5rem' className=' cursor-pointer' />
+      <CiHeart size='1.5rem' className='ml-4 cursor-pointer' />
+      <CiShoppingCart size='1.5rem' className='ml-4 cursor-pointer' />
+    </div>
+  );
+};
+
+export default TopNavLinks;
